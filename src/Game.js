8b29b157f28735c216c2925f4dc84a93dedda343
prakashlohanathan <prakash2k2mech@gmail.com>
2024-10-20 17:16:56 +0530

    current round update

--- src/Game.js
+++ src/Game.js
@@ -66,7 +66,7 @@ const Game = () => {
         />
       </div>
       <div>
-        <h2>Total 6 Rounds.Round: {round}</h2>
+        <h2>Total 6 Rounds.CurrentRound: {round}</h2>
         <div>
           <h3>{player1Name}</h3>
           {choices.map((choice) => (
